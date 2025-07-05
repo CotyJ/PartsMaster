46cd3014beb02cfb17d57f63947478c6c3c231e7
Coty Janeway <coty.janeway@gmail.com>
2025-07-05 09:30:58 -0700

    add basic scripts and slight cleanup

---
+++ scripts/convert_xlsx_to_csv.py
@@ -0,0 +1,15 @@
+# import pandas as pd
+
+def test():
+  print("Hi, this script works!")
+
+
+# def convert_excel_to_csv(input_path, output_path):
+#   df = pd.read_excel(input_path)
+#   df.to_csv(output_path)
+#   print("Converted!")
+
+
+if __name__ == "__main__":
+  test()
+#   convert_excel_to_csv()
---
+++ scripts/main.py
@@ -0,0 +1,14 @@
+
+
+# from convert_excel_to_csv.py import convert_excel_to_csv
+from convert_excel_to_csv.py import test
+
+
+
+# excel_file_path = 'C:/Users/Falco/Desktop/JL_DATA/Parts.xlsx'
+# csv_path = 'C:/Users/Falco/Desktop/JL_DATA/Parts_converted.csv'
+
+
+if __name__ == "__main__":
+  test()
+  # convert_excel_to_csv(excel_file_path, csv_path)
