24b37a81f0bdcff17d6e4fb8f6bb92d7dfcbc56f
Coty Janeway <coty.janeway@gmail.com>
2025-07-05 12:18:52 -0700

    modify so scripts also delete headers

--- scripts/convert_xlsx_to_csv.py
+++ scripts/convert_xlsx_to_csv.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def convert_xlsx_to_csv(input_path, output_path):
   df = pd.read_excel(input_path)
+  df = df.iloc[1:]
   df.to_csv(output_path)
 
 
--- scripts/main.py
+++ scripts/main.py
@@ -3,11 +3,11 @@ import pandas as pd
 from convert_xlsx_to_csv import convert_xlsx_to_csv
 
 
-# Just renames CSV for consistency
-def rename_csv(old_path, new_path):
+# Renames CSV for consistency and removes header, since I am defining the columns in postgres
+def rename_csv_remove_header(old_path, new_path):
   df = pd.read_csv(old_path)
+  df = df.iloc[1:]
   df.to_csv(new_path)
-
 
 excel_file_path = "../data/raw"
 csv_path = "../data/processed"
@@ -18,16 +18,16 @@ raw_parts_path = f"{excel_file_path}/Parts.xlsx"
 raw_production_models_path = f"{excel_file_path}/Production_models.csv"
 raw_whereused_path = f"{excel_file_path}/WhereUsed.xlsx"
 
-processed_orders_line_item_path = f"{csv_path}/OrdersLineItem.csv"
+processed_orders_line_item_path = f"{csv_path}/orders_line_item.csv"
 processed_overstock_path = f"{csv_path}/overstock.csv"
-processed_parts_path = f"{csv_path}/Parts.csv"
-processed_production_models_path = f"{csv_path}/Production_models.csv"
-processed_whereused_path = f"{csv_path}/WhereUsed.csv"
+processed_parts_path = f"{csv_path}/parts.csv"
+processed_production_models_path = f"{csv_path}/production_models.csv"
+processed_whereused_path = f"{csv_path}/where_used.csv"
 
 
 if __name__ == "__main__":
   convert_xlsx_to_csv(raw_orders_line_item_path, processed_production_models_path)
-  rename_csv(raw_overstock_path, processed_overstock_path)
+  rename_csv_remove_header(raw_overstock_path, processed_overstock_path)
   convert_xlsx_to_csv(raw_parts_path, processed_parts_path)
-  rename_csv(raw_production_models_path, processed_production_models_path)
+  rename_csv_remove_header(raw_production_models_path, processed_production_models_path)
   convert_xlsx_to_csv(raw_whereused_path, processed_whereused_path)
