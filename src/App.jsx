a5a73d26ca61c5007bb11e46ef84ecee320fb843
Coty Janeway <coty.janeway@gmail.com>
2025-07-07 11:48:22 -0700

    connect front end to server and pull basic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-// import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
+import './App.css';
+import axios from 'axios';
+
+// const axios = require('axios');
 
 function App() {
 
+  const [search, setSearch] = useState(null);
+
+  const get_sample_part = () => {
+    axios.get('http://localhost:6969/search')
+      .then((response) => {
+        const { data } = response;
+        console.log(data[0].part_number)
+        setSearch(data[0].part_number)
+      })
+      .catch((err) => console.log(err, " Error on front-end request"))
+      .finally(() => console.log("done i guess"));
+  }
+
+  get_sample_part()
+
+
+    // useEffect( () => {
+    //
+    // }, [])
 
   return (
     <>
       <div>
-
+        <div>Hello</div>
+        <input type="search" id="search-bar"></input>
+        <button onClick={get_sample_part}>GET</button>
+        <div>{search}</div>
       </div>
     </>
   )
