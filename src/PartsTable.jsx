b003823457b3393e665dfb825e87852fad7ebb80
Coty Janeway <coty.janeway@gmail.com>
2025-07-07 16:40:02 -0700

    add bootstrap and popper, and very basic style

--- src/PartsTable.jsx
+++ src/PartsTable.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 
-export default function PartsTable({partData}) {
+export default function PartsTable({partData, setSearchEntry} ) {
   return (
     <div>
-        <div>Hello</div>
           <fieldset>
-            <legend>Search Bar</legend>
             <input type="search" id="search-bar" onChange={(e) => setSearchEntry(e.target.value)} placeholder='ex: 43205-2304'></input>
           </fieldset>
-          <div>
-            {partData.map( (part) => (
-              <div key={part.id}>{part.part_number}</div>
-              ))}
-          </div>
+
+          {/* Table begin */}
+          <table className='table table-dark table-striped'>
+            <thead>
+              <tr>
+                <th scope="col">Part Number</th>
+                <th scope="col">Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {partData.map( (part) => (
+                <tr key={part.id} className=''>
+                  <td>{part.part_number}</td>
+                  <td className=''>{part.part_description}</td>
+                </tr>
+                ))}
+            </tbody>
+          </table>
       </div>
   )
 }
