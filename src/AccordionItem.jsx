7cb422d119079eeacb64b3ef10337536248501b2
Coty Janeway <coty.janeway@gmail.com>
2025-07-30 13:47:07 -0700

    add checkmark for boolean vals

--- src/AccordionItem.jsx
+++ src/AccordionItem.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 
 export default function AccordionItem({ part }) {
-  console.log(Object.keys(part));
+  const filteredKeys = ['id', 'part_number', 'part_description', 'user_name'];
+  const booleanVals = ['dchapman_ok', 'dkrich_ok'];
 
   return (
     <div className="row">
-      {Object.keys(part).map((key) =>
-        part[key] ? (
-          <div key={key} className="col-6 mb-4">
-            <h4 style={{ borderBottom: '1px solid white' }}>{key}</h4>
-            <div>{part[key]}</div>
-          </div>
-        ) : null
-      )}
+      {Object.keys(part)
+        .filter((key) => !filteredKeys.includes(key))
+        .map((key) => {
+          const value = part[key];
+          let displayValue;
+          if (typeof value == 'boolean') {
+            displayValue = value ? <h5>✅</h5> : <h5>❌</h5>;
+          } else {
+            displayValue = value;
+          }
+
+          return displayValue ? (
+            <div key={key} className="col-6 mb-4">
+              <h4 style={{ borderBottom: '1px solid white' }}>{key}</h4>
+              <div>{displayValue}</div>
+            </div>
+          ) : null;
+        })}
     </div>
   );
 }
