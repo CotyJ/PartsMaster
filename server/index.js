99c51b430c4fb6273d8963f491fc6e585b6cc6a7
Coty Janeway <coty.janeway@gmail.com>
2025-07-30 20:25:28 -0700

    formatting only

--- server/index.js
+++ server/index.js
@@ -1,67 +1,68 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const db = require('../db');
-require('dotenv').config()
-const PORT = process.env.PORT || 9999
-var cors = require('cors')
+require('dotenv').config();
+const PORT = process.env.PORT || 9999;
+var cors = require('cors');
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // When making a build
 // app.use(express.static(path.join(__dirname, '../public')));
 
-
 // All parts for initial load
-app.get('/search_parts/all', async (req,res) => {
+app.get('/search_parts/all', async (req, res) => {
   try {
-    const results = await db.query(`SELECT * FROM parts WHERE part_name != 'DNS' ORDER BY part_number limit 100`)
+    const results = await db.query(
+      `SELECT * FROM parts WHERE part_name != 'DNS' ORDER BY part_number limit 100`
+    );
     res.json(results.rows);
   } catch (err) {
     console.log(err);
-
   }
-
-})
+});
 
 // Search
 app.get('/search_parts', async (req, res) => {
   console.log('Searching...');
 
   try {
-  const { q } = req.query;
-  const results = await db.query(`
+    const { q } = req.query;
+    const results = await db.query(
+      `
     SELECT * FROM parts
     WHERE part_number ILIKE $1
     OR part_description ILIKE $1
     ORDER BY part_number
     LIMIT 100
-    `, [`%${q}%`]);
+    `,
+      [`%${q}%`]
+    );
 
     console.log(`Searching for ${q}`);
     res.json(results.rows);
-
   } catch (err) {
-    console.error("ERROR");
-    res.status(500).json({ error: 'Server error...'});
+    console.error('ERROR');
+    res.status(500).json({ error: 'Server error...' });
   }
 });
 
 // Get Where Used
-app.get('/search_where_used', async (req,res) => {
+app.get('/search_where_used', async (req, res) => {
   try {
     const { q } = req.query;
-    const results = await db.query(`SELECT bom_model FROM where_used WHERE part_number = $1 GROUP BY bom_model`, [`${q}`]);
-    res.json(results.rows)
-
+    const results = await db.query(
+      `SELECT bom_model FROM where_used WHERE part_number = $1 GROUP BY bom_model`,
+      [`${q}`]
+    );
+    res.json(results.rows);
   } catch (error) {
-      console.error("ERROR");
-      res.status(500).json({ error: 'Server error...'});
-
+    console.error('ERROR');
+    res.status(500).json({ error: 'Server error...' });
   }
-})
-
+});
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
