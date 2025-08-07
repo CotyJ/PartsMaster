ca94eff0024272905debdb395a81f0d7d42e0ea0
Coty Janeway <coty.janeway@gmail.com>
2025-08-07 10:03:31 -0700

    add placeholders for incomplete pages in the works

--- src/Inventory.jsx
+++ src/Inventory.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export default function Inventory() {
-  return (
-    <div>Inventory</div>
-  )
+  return <div className="bd-callout bd-callout-primary">Coming Soon!</div>;
 }
