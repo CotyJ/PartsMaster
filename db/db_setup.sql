27c77146d8b078ea105728ae9873793223f92271
Coty Janeway <coty.janeway@gmail.com>
2025-08-09 13:09:01 -0700

    better paths to run files from root

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -1,8 +1,7 @@
 -- Execute all sql files in /db
-\i './parts_table_schema.sql'
-\i './where_used_table_schema.sql'
-\i './inventory_table_schema.sql'
-\i './production_models_table_schema.sql'
-\i './orders_table_schema.sql'
-\i './kanban_table_schema.sql'
-
+\i 'db/parts_table_schema.sql'
+\i 'db/where_used_table_schema.sql'
+\i 'db/inventory_table_schema.sql'
+\i 'db/production_models_table_schema.sql'
+\i 'db/orders_table_schema.sql'
+\i 'db/kanban_table_schema.sql'
