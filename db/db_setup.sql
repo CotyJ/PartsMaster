6356a737dba7b133eedfc16f666940f8b89a56d6
Coty Janeway <coty.janeway@gmail.com>
2025-08-09 12:53:22 -0700

    add file to start seeding

---
+++ db/db_setup.sql
@@ -0,0 +1,8 @@
+-- Execute all sql files in /db
+\i './parts_table_schema.sql'
+\i './where_used_table_schema.sql'
+\i './inventory_table_schema.sql'
+\i './production_models_table_schema.sql'
+\i './orders_table_schema.sql'
+\i './kanban_table_schema.sql'
+
