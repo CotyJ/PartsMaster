16fcd490043d8290e8ea1349d0be2151cb6266d6
Coty Janeway <coty.janeway@gmail.com>
2025-08-09 12:39:32 -0700

    comment out old real data inserts for now

--- db/kanban_table_schema.sql
+++ db/kanban_table_schema.sql
@@ -6,15 +6,15 @@ CREATE TABLE kanban_cards (
   date_added DATE DEFAULT DATE(CURRENT_TIMESTAMP)
 );
 
--- Fake data to populate
-INSERT INTO kanban_cards (part_number)
-VALUES ('12201-4322' );
+-- Fake data to populate - // FIXME:  doesn't work with new fake data
+-- INSERT INTO kanban_cards (part_number)
+-- VALUES ('12201-4322' );
 
-INSERT INTO kanban_cards (part_number)
-VALUES ('43205-2304' );
+-- INSERT INTO kanban_cards (part_number)
+-- VALUES ('43205-2304' );
 
-INSERT INTO kanban_cards (part_number)
-VALUES ('98100-5801' );
+-- INSERT INTO kanban_cards (part_number)
+-- VALUES ('98100-5801' );
 
-INSERT INTO kanban_cards (part_number)
-VALUES ('64614-1825' );
+-- INSERT INTO kanban_cards (part_number)
+-- VALUES ('64614-1825' );
