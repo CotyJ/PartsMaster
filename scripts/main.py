83acdc96ecca552b44664eb5b98f8cc5294b7a0d
Coty Janeway <coty.janeway@gmail.com>
2025-08-09 12:01:49 -0700

    generate production models

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,8 @@
 import generate_parts
 import generate_where_used
+import generate_production_models
 
 if __name__ == "__main__":
   generate_parts.generate_parts_csv()
   generate_where_used.generate_where_used_csv()
+  generate_production_models.generate_production_models_csv()
