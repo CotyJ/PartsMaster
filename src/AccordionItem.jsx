873eb2ee7b2aacc8fa57ca8b86af198bb583ade9
Coty Janeway <coty.janeway@gmail.com>
2025-08-09 19:29:44 -0700

    add temporary status hooks and make space

--- src/AccordionItem.jsx
+++ src/AccordionItem.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 export default function AccordionItem({ part }) {
   const [whereUsed, setWhereUsed] = useState([]);
   const [expandedPartId, setExpandedPartId] = useState(null);
+  const [isOnOrder, setisOnOrder] = useState(false);
+  const [isInProduction, setisInProduction] = useState(false);
 
   const filteredKeys = ['id', 'part_number', 'part_description'];
   const BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -48,42 +50,78 @@ export default function AccordionItem({ part }) {
               borderRadius: '8px',
             }}
           >
-            <div className="col-2">
-              <h3 className="text-center">Used in...</h3>
-              <ul className="p-0">
-                {whereUsed
-                  .filter((item) => item.bom_model !== null)
-                  .map((item) => (
-                    <li
-                      key={item.bom_model}
-                      className="text-center"
-                    >{`${item.bom_model}`}</li>
-                  ))}
-              </ul>
-            </div>
-            <div className="col-10">
-              <div className="row">
-                {Object.keys(part)
-                  .filter((key) => !filteredKeys.includes(key))
-                  .map((key) => {
-                    const value = part[key];
-                    let displayValue;
-                    if (typeof value == 'boolean') {
-                      displayValue = value ? <h5>✅</h5> : <h5>❌</h5>;
-                    } else {
-                      displayValue = value;
-                    }
 
-                    return displayValue ? (
-                      <div key={key} className="col-6 mb-4">
-                        <h4 style={{ borderBottom: '1px solid white' }}>
-                          {key}
-                        </h4>
-                        <div style={{ overflow: 'auto' }}>{displayValue}</div>
-                      </div>
-                    ) : null;
-                  })}
+
+            {/* Dropdown section */}
+
+            <div className="row flex-wrap">
+
+              {/* Where Used section */}
+              <div className="col-2 col-sm-6 col-md-6 col-lg-2 order-1">
+                <h4
+                  className="text-center"
+                  style={{ borderBottom: '1px solid white' }}
+                >
+                  Used in
+                </h4>
+                <ul className="p-0">
+                  {whereUsed
+                    .filter((item) => item.bom_model !== null)
+                    .map((item) => (
+                      <li
+                        key={item.bom_model}
+                        className="text-center"
+                      >{`${item.bom_model}`}</li>
+                    ))}
+                </ul>
               </div>
+
+              {/* Status section */}
+              <div className="col-2 col-sm-6 col-md-6 col-lg-2 order-2">
+                <h4
+                  className="text-center"
+                  style={{ borderBottom: '1px solid white' }}
+                >
+                  Status
+                </h4>
+                <ul className='list-unstyled px-auto'>
+                  <li className='text-center'>{isInProduction ? <h5 className='text-nowrap'>✅ In production</h5> : <h5 className='text-nowrap fw-bold'>❌ OBSOLETE</h5>}</li>
+                  <li className='text-center'>on order</li>
+                  <li className='text-center'>
+                    <button className="btn btn-primary" disabled={isOnOrder}>
+                      Replenish
+                    </button>
+                  </li>
+                  <li></li>
+                </ul>
+              </div>
+
+              {/* Details section */}
+              <div className="col-8 order-3 order-sm-3 order-md-3 col-lg-8 col-md-12 col-sm-12">
+                <div className="row">
+                  {Object.keys(part)
+                    .filter((key) => !filteredKeys.includes(key))
+                    .map((key) => {
+                      const value = part[key];
+                      let displayValue;
+                      if (typeof value == 'boolean') {
+                        displayValue = value ? <h5>✅</h5> : <h5>❌</h5>;
+                      } else {
+                        displayValue = value;
+                      }
+
+                      return displayValue ? (
+                        <div key={key} className="col-5 mb-4 ms-4">
+                          <h4 style={{ borderBottom: '1px solid white' }}>
+                            {key}
+                          </h4>
+                          <div style={{ overflow: 'auto' }}>{displayValue}</div>
+                        </div>
+                      ) : null;
+                    })}
+                </div>
+              </div>
+
             </div>
           </div>
         </td>
